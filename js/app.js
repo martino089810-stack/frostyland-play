866bf8ee6912f50e7f341af6d88c719b32d56534
martino089810-stack <martino089810@gmail.com>
2026-07-02 15:48:30 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let juegoFinalizado = false;
+
 //====================================================
 // FROSTYLAND PLAY
 // Descongela el Boli
@@ -9,14 +11,14 @@
 // AUDIO
 //====================================================
 
-const sndClick = new Audio("CLIC.mp3");
-const sndIce = new Audio("ICE.mp3");
-const sndWin = new Audio("WIN.mp3");
-const sndLose = new Audio("LOSE.mp3");
+const sndClick = new Audio("audio/CLIC.mp3");
+const sndIce = new Audio("audio/ICE.mp3");
+const sndWin = new Audio("audio/WIN.mp3");
+const sndLose = new Audio("audio/LOSE.mp3");
 
 sndClick.volume = 0.7;
 sndIce.volume = 0.25;
-sndWin.volume = 0.9;
+sndWin.volume = 0.4;
 sndLose.volume = 0.8;
 
 
@@ -40,21 +42,21 @@ const premios = [
 
 {
     nombre:"BOLI",
-    imagen:"premio-Boli.png",
+    imagen:"img/premio-Boli.png",
     titulo:"¡¡FELICIDADES!!",
     texto:"Ganaste un Boli Bubulubú 🍧"
 },
 
 {
     nombre:"CUPON",
-    imagen:"premio-cupon.png",
+    imagen:"img/premio-cupon.png",
     titulo:"¡¡GENIAL!!",
     texto:"Ganaste un Cupón 2x1 🎁"
 },
 
 {
     nombre:"PERDER",
-    imagen:"premio-perder.png",
+    imagen:"img/premio-perder.png",
     titulo:"CASI LO LOGRAS",
     texto:"Más suerte para la próxima ❄"
 }
@@ -115,7 +117,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 const hielo = new Image();
-hielo.src = "hielo.png";
+hielo.src = "img/hielo.png";
 
 hielo.onload = function(){
 
@@ -158,6 +160,7 @@ document.getElementById("btnInicio").onclick = ()=>{
 document.getElementById("btnJugar").onclick = ()=>{
 
     sndClick.play();
+    sndWin.play();
 
     premioActual = obtenerPremio();
 
@@ -178,6 +181,18 @@ document.getElementById("btnJugar").onclick = ()=>{
 
         ctx.clearRect(0,0,canvas.width,canvas.height);
 
+        juegoFinalizado = false;
+
+porcentajeDescubierto = 0;
+
+raspando = false;
+
+document.getElementById("premio").classList.add("oculto");
+
+document.getElementById("progreso").style.width = "0%";
+
+document.getElementById("porcentaje").innerHTML = "0%";
+
         ctx.drawImage(
             hielo,
             0,
@@ -190,20 +205,206 @@ document.getElementById("btnJugar").onclick = ()=>{
 
 };
 
+//==============================
+// VARIABLES DEL RASPADO
+//==============================
+
+let raspando = false;
+
+let porcentajeDescubierto = 0;
+
+let sonidoHielo = false;
+
+canvas.addEventListener("mousedown", iniciarRaspado);
+
+canvas.addEventListener("mouseup", terminarRaspado);
+
+canvas.addEventListener("mousemove", raspar);
+
+canvas.addEventListener("touchstart", iniciarRaspado);
+
+canvas.addEventListener("touchend", terminarRaspado);
+
+canvas.addEventListener("touchmove", raspar);
+
+function iniciarRaspado(){
+
+    raspando=true;
+
+    if(!sonidoHielo){
+
+    sndIce.loop=true;
+
+    sndIce.play();
+
+    sonidoHielo=true;
+
+}
+
+}
+
+function terminarRaspado(){
+
+    raspando = false;
+
+    sndIce.pause();
+
+    sndIce.currentTime = 0;
+
+    sonidoHielo = false;
+
+}
+
+function raspar(e){
+
+    if(!raspando) return;
+
+    let rect = canvas.getBoundingClientRect();
+
+    let x;
+    let y;
+
+    if(e.touches){
+
+        x = e.touches[0].clientX - rect.left;
+        y = e.touches[0].clientY - rect.top;
+
+    }else{
+
+        x = e.clientX - rect.left;
+        y = e.clientY - rect.top;
+
+    }
+
+    ctx.globalCompositeOperation = "destination-out";
+
+    ctx.beginPath();
+
+    ctx.arc(x,y,22,0,Math.PI*2);
+
+    ctx.fill();
+
+    calcularRaspado();
+
+    document.getElementById("progreso").style.width =
+    (porcentajeDescubierto*100)+"%";
+
+    document.getElementById("porcentaje").innerHTML =
+    Math.floor(porcentajeDescubierto*100)+"%";
+
+    if(porcentajeDescubierto >= 0.70 && !juegoFinalizado){
+
+        juegoFinalizado = true;
+
+        finalizarJuego();
+
+    }
+
+}
+
+document
+.getElementById("progreso")
+.style.width=(porcentajeDescubierto*100)+"%";
+
+document
+.getElementById("porcentaje")
+.innerHTML=
+Math.floor(porcentajeDescubierto*100)+"%";
+
+if(porcentajeDescubierto >= 0.70 && !juegoFinalizado){
+
+    juegoFinalizado = true;
+
+    function finalizarJuego(){
+
+    raspando = false;
+
+    sndIce.pause();
+    sndIce.currentTime = 0;
+
+    if(premioActual.nombre==="PERDER"){
+
+        sndLose.currentTime = 0;
+        sndLose.play();
+
+    }else{
+
+        sndWin.currentTime = 0;
+        sndWin.play();
+
+    }
+
+    document
+    .getElementById("premio")
+    .classList
+    .remove("oculto");
+
+}
+
+}
 
 
+
+//======================================
+// CALCULAR PORCENTAJE RASPADO
+//======================================
+
+function calcularRaspado(){
+
+    const imagen = ctx.getImageData(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    let pixelesTransparentes = 0;
+
+    for(let i=3;i<imagen.data.length;i+=4){
+
+        if(imagen.data[i]===0){
+
+            pixelesTransparentes++;
+
+        }
+
+    }
+
+    porcentajeDescubierto =
+        pixelesTransparentes/
+        (canvas.width*canvas.height);
+
+}
+
+
+// FINAL DEL JUEGO
+//=====================================
+function finalizarJuego(){
+
+    raspando=false;
+
+    sndIce.pause();
+
+    sndIce.currentTime=0;
+
+    if(premioActual.nombre==="PERDER"){
+
+        sndLose.play();
+
+    }
+
+    else{
+
+        sndWin.play();
+
+    }
+
+    document
+    .getElementById("premio")
+    .classList
+    .remove("oculto");
+
+}
+//
 //====================================================
-// AQUÍ CONTINUAREMOS
-//====================================================
-//
-// RASPADO
-//
-// SONIDO DEL HIELO
-//
-// DETECTAR PORCENTAJE
-//
-// MOSTRAR PREMIO
-//
-// ANIMACIÓN FINAL
-//
-//====================================================
+
